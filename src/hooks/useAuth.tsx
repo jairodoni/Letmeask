4d706b2fa0809f4a543b706bb794610a5600278e
Jairo Doni <jairo.doni97@gmail.com>
2021-06-23 22:00:51 -0300

    Tela de Authenticação e de Criar sala + Firebase + useContext

---
+++ src/hooks/useAuth.tsx
@@ -0,0 +1,75 @@
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import { auth, firebase } from '../services/firebase'
+
+interface User {
+  id: string;
+  name: string;
+  avatar: string;
+}
+
+interface AuthContextType {
+  user: User | undefined;
+  signInWithGoogle: () => Promise<void>;
+}
+
+interface AuthContextProviderProps {
+  children: ReactNode;
+}
+
+const AuthContext = createContext({} as AuthContextType);
+
+export function AuthContextProvider({ children }: AuthContextProviderProps) {
+  const [user, setUser] = useState<User>()
+
+  async function signInWithGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    const result = await auth.signInWithPopup(provider);
+
+    if (result.user) {
+      const { displayName, photoURL, uid } = result.user;
+
+      if (!displayName || !photoURL) {
+        throw new Error('Missing information from Google Account.')
+      }
+
+      setUser({
+        id: uid,
+        name: displayName,
+        avatar: photoURL,
+
+      })
+    }
+
+  }
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if (user) {
+        const { displayName, photoURL, uid } = user;
+
+        if (!displayName || !photoURL) {
+          throw new Error('Missing information from Google Account.')
+        }
+
+        setUser({
+          id: uid,
+          name: displayName,
+          avatar: photoURL,
+        })
+      }
+    })
+    //Esse return serve para fazer com que ele pare de ouvir(checar) se tem
+    // alguem logado na comta ao final do useEffect, visto como uma boa pratica
+
+    return () => {
+      unsubscribe();
+    }
+  }, [])
+  return (
+    <AuthContext.Provider value={{ user, signInWithGoogle }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export const useAuth = () => useContext(AuthContext);
